8f4c0c17058389d6e9bfb6034ae8e7031df26b8f
Eriquy Jhordan <eriquy@gmail.com>
2022-05-16 01:15:42 -0300

    feat: create user migration

---
+++ database/migrations/1652454862139_users.ts
@@ -0,0 +1,27 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Users extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary()
+      table.string('name').notNullable()
+      table.string('email').notNullable()
+      table.string('password').notNullable()
+      table.string('phone')
+      table.string('avatar')
+      table.boolean('is_active').defaultTo(true)
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.dateTime('created_at', { useTz: true })
+      table.dateTime('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
