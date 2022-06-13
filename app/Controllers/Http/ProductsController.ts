22d49d7567b76fe02c6102f6ca0fca3822424916
Eriquy Jhordan <eriquy@gmail.com>
2022-06-13 14:38:24 -0300

    feat: create products through a file

--- app/Controllers/Http/ProductsController.ts
+++ app/Controllers/Http/ProductsController.ts
@@ -1,6 +1,8 @@
 import BadResquestException from 'App/Exceptions/BadResquestException'
+
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { v4 as uuid } from 'uuid'
+import excelToJson from 'convert-excel-to-json'
 import Product from 'App/Models/Product'
 
 export default class ProductsController {
@@ -32,6 +34,63 @@ export default class ProductsController {
     })
 
     return response.json(product)
+  }
+
+  public async uploadFile({ request, response }: HttpContextContract) {
+    const file = request.file('file')
+    if (file) {
+      const fileName = `${uuid()}.${file.extname}`
+      await file.move(`${__dirname}/../../../tmp/files`, {
+        name: fileName,
+      })
+      const result = excelToJson({
+        sourceFile: `${__dirname}/../../../tmp/files/${fileName}`,
+        sheets: ['Pre Pedido'],
+      })
+      const products = result['Pre Pedido'].map((product) => {
+        if (
+          product['D'] &&
+          product['A'] &&
+          typeof product['A'] === 'string' &&
+          typeof product['G'] === 'number'
+        ) {
+          return {
+            name: product['D'],
+            code: product['A'],
+            quantity: product['G'],
+            unitPrice: product['I'],
+            totalPrice: product['J'],
+            boxes: product['K'],
+            grossWeight: product['M'],
+            liquidWeight: product['N'],
+          }
+        }
+        return null
+      })
+      const productsWithoutNull = products.filter((product) => product)
+      await Promise.all(
+        productsWithoutNull.map(async (product) => {
+          const productAlreadyExists = await Product.findBy('code', product['code'])
+          if (productAlreadyExists) {
+            productAlreadyExists.merge(product)
+            return await productAlreadyExists.save()
+          }
+          const productId = uuid()
+          const savedProduct = await Product.create({
+            id: productId,
+            ...product,
+          })
+          return savedProduct
+        })
+      )
+
+      return response.json({
+        success: true,
+      })
+    }
+    return response.status(400).json({
+      error: 'File not found',
+    })
   }
 
   public async index({ response }: HttpContextContract) {
